1d3e4c6c7668a717ce71f509271e8a3a418387e9
vertally <vgtaleon@gmail.com>
2024-05-08 15:14:43 -0400

    C# practice with print line, operators, variables, etc.

---
+++ ToDoList/ToDoList/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs.");
+Console.WriteLine("[A]dd a TODO.");
+Console.WriteLine("[R]emove a TODO.");
+Console.WriteLine("[E]xit.");
+
+// READING USER INPUT
+Console.WriteLine("Type:");
+string userChoice = Console.ReadLine();
+Console.WriteLine("User input: " + userChoice);
+
+// USER PRESSES ANY KEY TO CONTINUE RUNNING THE CODE
+Console.ReadKey();
+
+// VARIABLE DECLARATION AND INITIALIZATION
+string userInput = "A";
+Console.WriteLine(userInput);
+
+// OPERATORS
+int a = 2;
+int b = 5;
+
+Console.WriteLine("Addition: " + (a + b));
+Console.WriteLine("Subtraction: " + (a - b));
+Console.WriteLine("Multiplication: " + a * b);
+Console.WriteLine("Division: " + a / b);
+
+// TO INCREMENT VALUES BY ONE:
+++a;
+--b;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+// EXPLICIT VARIABLES
+string word = "ABC";
+int number = 5;
+
+// IMPLICIT VARAIABLES
+var wordVar = "ABC";
+var numberVar = 5;
